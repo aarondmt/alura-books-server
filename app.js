4d835c946264f5fe3ab2e1619180a75fdf05a712
Aaron Lesbão Dumont <aarondmt@gmail.com>
2024-02-05 23:18:28 -0400

    Adição do CORS para permissão de requisição.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const routeLivro = require("./routes/livro.js");
+const cors = require("cors");
 
 const app = express();
 
 const port = 8000;
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 app.use("/livros", routeLivro);
 
